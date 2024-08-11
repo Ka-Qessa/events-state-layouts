b23c6c415c79533556d35e914f0383039691c875
Irina Shibaeva <irina-kuppieva@yandex.ru>
2024-08-11 23:30:32 +0300

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-# events-state-layouts
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Store from './layouts/Store';
+import products from './layouts/data/products';
+
+function App() {
+  return (
+    <div>
+      <Store products={products} />
+    </div>
+  );
+}
+
+export default App;
